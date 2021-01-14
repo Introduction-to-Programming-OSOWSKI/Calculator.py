ab45608692baa56428482cd4ca8e7f8b843fc2da
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2021-01-14 08:35:29 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,17 +10,17 @@ month = 1
 day = 14
 
 def test_code():
-    assert main.calculate("add", 2, 2) == 4
-    assert main.calculate("add", 10, 2) == 12
+    assert main.calculate("add", 2, 2) == 4, "2 + 2 == 10 failed"
+    assert main.calculate("add", 10, 2) == 12, "10 + 2 == 12 failed"
 
-    assert main.calculate("subtract", 2, 2) == 0
-    assert main.calculate("subtract", 10, 5) == 5
+    assert main.calculate("subtract", 2, 2) == 0, "2 - 2 == 0 failed"
+    assert main.calculate("subtract", 10, 5) == 5, "10 - 5 == 5 failed"
     
-    assert main.calculate("multiply", 2, 3) == 6
-    assert main.calculate("multiply", 100, 5) == 500
+    assert main.calculate("multiply", 2, 3) == 6, "2 * 3 == 6 failed"
+    assert main.calculate("multiply", 100, 5) == 500, "100 * 5 == 500 failed"
     
-    assert main.calculate("divide", 5, 2) == 2.5
-    assert main.calculate("divide", 20, 2) == 10.0
+    assert main.calculate("divide", 5, 2) == 2.5, "5 / 2 == 2.5 failed"
+    assert main.calculate("divide", 20, 2) == 10.0, "20 / 2 == 10.0 failed"
 
 def test_late():
     assert datetime.datetime.now() < datetime.datetime(year, month, day + 1, 4, 0), "Submitted Late"
